391c8307ae0134c273e8e7b4dd5e4f2bfd618892
WoleIlori <gideonilori@yahoo.com>
2018-02-01 17:05:38 +0000

    Get attractions closest to the user using max heap algorithm

--- app/src/main/java/com/example/wollyz/futouristic/Find_Nearby_Tourist_Handler.java
+++ app/src/main/java/com/example/wollyz/futouristic/Find_Nearby_Tourist_Handler.java
@@ -13,26 +13,28 @@ public class Find_Nearby_Tourist_Handler {
     private UserLocationHandler locManager;
     private double[] distance; //distance for attraction
     private int[] attractionIndex;
-    private String[] name;
+    //private NearbyAttraction nearby;
     private int heapSize; //size of k ie. kth nearest attractions
     private Context c;
-    //private MaxHeap maxheap;
+    private MaxHeap maxheap;
 
 
 
     Find_Nearby_Tourist_Handler(double latitude, double longitude){ //double latitude, double longitude
-        heapSize = 4;
+        heapSize = 3;
         this.latitude = latitude;
         this.longitude = longitude;
-        //maxheap = new MaxHeap(heapSize);
+        maxheap = new MaxHeap(heapSize);
         attractionIndex = new int[heapSize + 1];
+        //nearby = new NearbyAttraction();
     }
 
     public void getNearestAttractions(List<Attractions> attractions)
     {
         //latitude = locManager.getUserLatitude();
         //longitude = locManager.getUserLongitude();
-
+        String name;
+        double dist;
         distance = new double[attractions.size()];
         int index;
 
@@ -43,6 +45,26 @@ public class Find_Nearby_Tourist_Handler {
         for(int i = 0; i < attractions.size(); i++){
             distance[i] = Harvesine.calculateDist(latitude,longitude,attractions.get(i));
             System.out.println(distance[i]);
+        }
+
+        //add distance to max heap
+        for(int i = 0; i < distance.length; i++){
+            maxheap.insert(distance[i], i);
+        }
+
+        //get the index of attractions nearby
+        attractionIndex = maxheap.getHeap();
+
+        System.out.println("nearest attractions:");
+        for(int i = 1; i < heapSize + 1; i++)
+        {
+            index = attractionIndex[i];
+            name = attractions.get(index).getName();
+            //nearby.setAttraction(name);
+            dist = distance[index];
+            //nearby.setDistance(dist);
+            System.out.println(name + ": " + dist);
+
         }
 
     }
---
+++ app/src/main/java/com/example/wollyz/futouristic/MaxHeap.java
@@ -0,0 +1,146 @@
+package com.example.wollyz.futouristic;
+
+/**
+ * Created by Wollyz on 01/02/2018.
+ */
+public class MaxHeap {
+    private double[] heap;
+    private int[] distPos;
+    private int maxSize;
+    private int size;
+    private static final int ROOT = 1;
+    private boolean notFull;
+    private int equalNodes; //used to check if subtrees have equal children
+
+    MaxHeap(int maxSize)
+    {
+        this.maxSize = maxSize;
+        size = 0;
+        notFull = true;
+        heap = new double[maxSize + 1];
+        distPos = new int[maxSize+ 1];
+        heap[0] = Double.MAX_VALUE;
+        distPos[0] = Integer.MAX_VALUE;
+    }
+
+    //parent index
+    public int parent(int pos)
+    {
+        return pos / 2;
+    }
+
+    //left child index
+    public int leftChild(int pos){
+        return pos * 2;
+    }
+
+    //right child index
+    public int rightChild(int pos){
+        return( pos * 2) + 1;
+    }
+
+/*
+    private boolean hasRightChild(int pos){
+        if(leftChild(pos) == equalNodes){  //size
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+*/
+
+
+
+
+    //c_pos child pos, p_pos parent pos
+    private void swap(int c_pos, int p_pos){
+        double tmp;
+        int tmpIndex;
+
+        tmp = heap[c_pos];
+        tmpIndex = distPos[c_pos];
+
+        heap[c_pos] = heap[p_pos];
+        distPos[c_pos] = distPos[p_pos];
+
+        heap[p_pos] = tmp;
+        distPos[p_pos] = tmpIndex;
+    }
+
+    public void insert(double dist, int distIndex){
+        if(notFull) {
+            size += 1;
+            heap[size] = dist;
+            distPos[size] = distIndex;
+            heapifyUp(size);
+            if (size == maxSize) {
+                notFull = false;
+            }
+
+
+        }
+        else
+        {
+            if(dist < heap[ROOT]){
+                heap[ROOT] = dist;
+                distPos[ROOT] = distIndex;
+                buildMaxHeap();
+            }
+        }
+
+    }
+
+    public void heapifyUp(int pos){
+        int current = pos;
+        while(heap[current] > heap[parent(current)]){
+            swap(current, parent(current));
+            current = parent(current);
+        }
+
+    }
+
+    public void maxHeapify(int pos){
+        int largest = pos;
+        if(leftChild(pos) <= size && heap[pos] < heap[leftChild(pos)]){
+            largest = leftChild(pos);
+        }
+
+        if(rightChild(pos) <= size && heap[pos] < heap[rightChild(pos)]){
+            if(heap[largest] < heap[rightChild(pos)]){
+                largest = rightChild(pos);
+            }
+        }
+
+
+        if(largest != pos){
+            swap(largest,pos);
+            maxHeapify(largest);
+
+        }
+
+
+    }
+
+    public void buildMaxHeap(){
+        for(int pos =  size / 2; pos>=1; pos--){
+            maxHeapify(pos);
+        }
+    }
+
+    public int[] getHeap(){
+        return distPos;
+    }
+
+    /*
+    distPos[size] = distIndex;
+          if (size == maxSize) {
+              maxAlgorithm();
+              full = true;
+          }*/
+
+
+
+
+
+}
